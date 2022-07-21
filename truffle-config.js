8b3c42d98a6395f81012916adcaa3c75728a6f7e
Max Aussem <aussem.maxwell@gmail.com>
2022-07-21 13:54:35 -0400

    added emerald test and main to config

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,19 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
+    emerald_testnet: {
+      url: "https://testnet.emerald.oasis.dev",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        network_id: "*", // Match any network id
+        from: "0x0000000000000000000000000000000000000001"
+    },
+    emerald_mainnet: {
+      url: "https://emerald.oasis.dev",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        network_id: "*" // Match any network id
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
